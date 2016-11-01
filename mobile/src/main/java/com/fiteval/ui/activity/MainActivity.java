b0aa053f651df28e53730d8e8718f1cae9a13906
mikz <mikias@outlook.com>
2016-11-01 05:36:13 -0400

    signin and database backend logic

--- mobile/src/main/java/com/fiteval/ui/activity/MainActivity.java
+++ mobile/src/main/java/com/fiteval/ui/activity/MainActivity.java
@@ -1,190 +1,112 @@
 package com.fiteval.ui.activity;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.balysv.materialmenu.MaterialMenuDrawable;
 import com.fiteval.R;
-import com.fiteval.ui.fragment.NavigationFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,
-        AdapterView.OnItemSelectedListener,
-        DrawerLayout.DrawerListener,
-        NavigationFragment.Callback {
+/**
+ * Created by Mikias Alemu on 11/01/2016.
+ */
+public class MainActivity extends AppCompatActivity {
 
-    private NavigationFragment mNavigationFragment;
-    private DrawerLayout mDrawerLayout;
-    private MaterialMenuDrawable mMaterialMenu;
-    private Toolbar mToolbar;
+    //UI controls
+    private Button mSignin, mSignup, mForgotPassword;
+    private EditText mEmail, mPassword;
+    private ProgressDialog mProgress;
 
-    // -------------------------------------------------------
-    private Context mContext;
-    private FragmentManager mFragManager;
-
+    //Firebase Auth class
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mContext = getApplicationContext();
-        mFragManager = getSupportFragmentManager();
-        setContentView(R.layout.activity_main);
-        initToolbar();
-    }
+        setContentView(R.layout.activity_login);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        initView();
-        initToolbar();
-    }
+        //UI components
+        mSignin = (Button) findViewById(R.id.signin);
+        mSignup = (Button) findViewById(R.id.signup);
+        mForgotPassword = (Button) findViewById(R.id.forgot);
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
 
-    private void initView() {
-        if (mNavigationFragment == null) {
-            mNavigationFragment = (NavigationFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.common_navigation_drawer);
+        mAuth = FirebaseAuth.getInstance();
+        mProgress = new ProgressDialog(this);
 
-            if (mNavigationFragment == null) {
-                mNavigationFragment = new NavigationFragment();
-            }
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.common_navigation_drawer, mNavigationFragment)
-                    .commit();
-        }
-
-        if (mDrawerLayout == null) {
-            mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_drawer_layout);
-            mDrawerLayout.setFocusable(false);
-            mDrawerLayout.setVisibility(View.VISIBLE);
-            mDrawerLayout.setDrawerListener(this);
-            mDrawerLayout.setScrimColor(ContextCompat.getColor(this, android.R.color.transparent));
-            mNavigationFragment.setUp(mDrawerLayout);
-        }
-    }
-
-    private void initToolbar() {
-        if (mToolbar == null) {
-            getToolbar();
-        }
-        mMaterialMenu = new MaterialMenuDrawable(MainActivity.this, Color.WHITE, MaterialMenuDrawable.Stroke.THIN);
-        mToolbar.setNavigationIcon(mMaterialMenu);
-        mToolbar.setTitleTextColor(Color.WHITE);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        mSignin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if (mNavigationFragment != null) {
-                    if (mNavigationFragment.isDrawerOpen()) {
-                        mNavigationFragment.closeDrawer();
-                        mMaterialMenu.animateIconState(MaterialMenuDrawable.IconState.BURGER);
-                    } else {
-                        mNavigationFragment.openDrawer();
-                        mMaterialMenu.animateIconState(MaterialMenuDrawable.IconState.ARROW);
-                    }
-                }
+            public void onClick(View view) {
+                Signin();
             }
         });
-        String title = (String) mToolbar.getTitle();
-        if (title != null) {
-            // DataUtil.getSpannableString(title, true);
-            mToolbar.setTitle(title);
-        }
-        setSupportActionBar(mToolbar);
-    }
 
-    protected Toolbar getToolbar() {
-        if (mToolbar == null) {
-            mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
-        }
-        return mToolbar;
-    }
+        mSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-    @Override
-    public void onDrawerSlide(View drawerView, float slideOffset) {
-        if (mNavigationFragment != null) {
-            mMaterialMenu.setTransformationOffset(
-                    MaterialMenuDrawable.AnimationState.BURGER_ARROW,
-                    mNavigationFragment.isDrawerOpen() ? 2 - slideOffset : slideOffset
-            );
-        }
-    }
+                Intent signup = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(signup);
 
-    @Override
-    public void onDrawerOpened(View drawerView) {
-        if (mNavigationFragment != null) {
-            mNavigationFragment.setDrawerOpen(true);
-        }
-    }
-
-    @Override
-    public void onDrawerClosed(View drawerView) {
-        if (mNavigationFragment != null) {
-            mNavigationFragment.setDrawerOpen(false);
-            mNavigationFragment.setOpenedByBackBtn(false);
-        }
-    }
-
-    @Override
-    public void onDrawerStateChanged(int newState) {
-        if (newState == DrawerLayout.STATE_IDLE) {
-            if (mNavigationFragment.isDrawerOpen()) {
-                mMaterialMenu.setIconState(MaterialMenuDrawable.IconState.ARROW);
-            } else {
-                mMaterialMenu.setIconState(MaterialMenuDrawable.IconState.BURGER);
             }
+        });
+
+        mForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent forgot = new Intent(MainActivity.this,ForgotPasswordActivity.class);
+                startActivity(forgot);
+
+            }
+        });
+
+
+    }
+
+    private void Signin() {
+
+        //Getting Email and password from users
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(MainActivity.this,"Please enter email", Toast.LENGTH_LONG).show();
+            return;
         }
-    }
 
-    @Override
-    public void onClick(View v) {
-
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    // =============================================================================================
-    @Override
-    public void navigationListOnClick(View v) {
-        switch (v.getId()) {
-            case R.id.fragment_navigation_drawer_btn_main:
-                toastCenter("MAIN Clicked");
-                break;
-
-            case R.id.fragment_navigation_drawer_btn_raid:
-                toastCenter("RAID Clicked");
-                break;
-
-            case R.id.fragment_navigation_drawer_btn_leaderboard:
-                toastCenter("LEADERBOARD Clicked");
-                break;
-
-            case R.id.fragment_navigation_drawer_btn_shop:
-                toastCenter("SHOP Clicked");
-                break;
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(MainActivity.this,"Please enter Password", Toast.LENGTH_LONG).show();
+            return;
         }
-    }
 
-    public void toastCenter(String msg) {
-        Toast toast = Toast.makeText(mContext, msg, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0, 0);
-        toast.show();
+        //If email and password provided display progress dialog
+        mProgress.setMessage("Signing in please wait.....");
+        mProgress.show();
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this,"Successfully signed in", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"There was an error....", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
     }
 }
