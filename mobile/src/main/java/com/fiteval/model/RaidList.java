0f90ef7e07583fa4e82c5c1ab702cf1deb36bbd8
TravisNguyen <nguyentrav@gmail.com>
2016-11-29 14:30:00 -0500

    Added four raid locations

--- mobile/src/main/java/com/fiteval/model/RaidList.java
+++ mobile/src/main/java/com/fiteval/model/RaidList.java
@@ -20,9 +20,32 @@ public class RaidList {
         burrussLoc.setLongitude(-80.422860);
         Raid burruss = new Raid("Burruss Hall", burrussLoc, R.drawable.burruss);
 
-        //TODO add more raid locations
+        Location cafeMekongLoc = new Location("");
+        cafeMekongLoc.setLatitude(37.216210);
+        cafeMekongLoc.setLongitude(-80.399864);
+        Raid cafeMekong = new Raid("Cafe Mekong", cafeMekongLoc, R.drawable.cafemekong);
+
+        Location laneStadiumLoc = new Location("");
+        laneStadiumLoc.setLatitude(37.216210);
+        laneStadiumLoc.setLongitude(-80.399864);
+        Raid laneStadium = new Raid("Lane Stadium", laneStadiumLoc, R.drawable.lanestadium);
+
+        Location mossArtCenterLoc = new Location("");
+        mossArtCenterLoc.setLatitude(37.231849);
+        mossArtCenterLoc.setLongitude(-80.418066);
+        Raid mossArtCenter = new Raid("Moss Art Center", mossArtCenterLoc, R.drawable.mossartcenter);
+
+
+        Location villageLocation = new Location("");
+        villageLocation.setLatitude(37.246264);
+        villageLocation.setLongitude(-80.428977);
+        Raid village = new Raid("Village", villageLocation, R.drawable.village);
 
         list.add(burruss);
+        list.add(cafeMekong);
+        list.add(laneStadium);
+        list.add(mossArtCenter);
+        list.add(village);
 
         return list;
     }
